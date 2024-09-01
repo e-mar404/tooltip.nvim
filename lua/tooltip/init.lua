07540ab34e858452c42b5d3cb9a9fa5aacec1142
emar <egmarinn@gmail.com>
2024-08-31 19:27:44 -0500

    add: tooltip.close() (and default <leader>q keymap)

--- lua/tooltip/init.lua
+++ lua/tooltip/init.lua
@@ -2,13 +2,15 @@ local util = require 'tooltip.util'
 
 local M = {}
 local patterns = {}
+local output_buffer
+local win_id
 
 M.setup = function (config)
   patterns = config['patterns']
 end
 
 M._run = function (command)
-  local output_buffer = tonumber(vim.api.nvim_create_buf(true, true))
+  output_buffer = tonumber(vim.api.nvim_create_buf(true, true))
 
   local command_table = util._table_of(command)
 
@@ -17,11 +19,9 @@ M._run = function (command)
   }):wait()
 
   util._write_to_buf(output_buffer, obj.stdout)
-
-  return output_buffer
 end
 
-M._open_win = function (output_buffer)
+M._open_win = function ()
   local lines = vim.api.nvim_buf_get_lines(output_buffer, 0, -1, true)
   local width = util._longest_line(lines)
   local height = vim.api.nvim_buf_line_count(output_buffer)
@@ -36,16 +36,27 @@ M._open_win = function (output_buffer)
     style = 'minimal',
   }
 
-  vim.api.nvim_open_win(output_buffer, true, opts)
+  win_id = vim.api.nvim_open_win(output_buffer, true, opts)
 end
 
 
 M.show = function ()
   local file = util._file_name(0)
   local command = util._command_for_file(file, patterns)
-  local output_buffer = M._run(command)
+  M._run(command)
 
-  M._open_win(output_buffer)
+  vim.api.nvim_buf_set_keymap(output_buffer, 'n', '<leader>q', '', {
+    callback = function ()
+      require('tooltip').close()
+    end
+  })
+
+  M._open_win()
+end
+
+M.close = function ()
+  vim.api.nvim_win_close(win_id, true)
+  vim.api.nvim_buf_delete(output_buffer, {})
 end
 
 M._clear = function ()
